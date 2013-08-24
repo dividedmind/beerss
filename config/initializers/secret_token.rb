c7e06371be26a4c8bf8f5aef16e47eca342d8bad
Rafał Rzepecki <divided.mind@gmail.com>
2013-08-24 14:45:11 +0200

    Remove the secret key base from the repo.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,8 @@
-# Be sure to restart your server when you modify this file.
+require 'securerandom'
 
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
+unless secret_key_base = ENV['SECRET_KEY_BASE']
+  Rails.logger.warn "No $SECRET_KEY_BASE set, generating a random one (this will invalidate all cookies when server restarts)"
+  secret_key_base = SecureRandom.hex(64)
+end
 
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Beerss::Application.config.secret_key_base = '030660e99c53f2a55af7f5be2a50966833a4f62ce2b7504618e75f291830299b69f81641153aa35d7a5fe83f4036527ddeca51a59fb409acbee23f44c78e40ca'
+Beerss::Application.config.secret_key_base = secret_key_base
