fdeef0249aaee248441ab65afe577b1435d86994
Rafał Rzepecki <divided.mind@gmail.com>
2013-08-24 12:49:51 +0200

    Just boilerplate.

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jbuilder', '~> 1.2'
+gem 'haml-rails'
+
+gem 'sequel-rails'
+gem 'pg'
+
+gem 'beeminder'
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beerss::Application.config.secret_key_base = '030660e99c53f2a55af7f5be2a50966833a4f62ce2b7504618e75f291830299b69f81641153aa35d7a5fe83f4036527ddeca51a59fb409acbee23f44c78e40ca'
